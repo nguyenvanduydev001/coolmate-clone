2bc26d400309c104b9dc06a581219df0a724f381
nguyenvanduydev001 <nguyenvanduydev001@gmail.com>
2024-06-07 11:28:04 +0700

    Giao diện trang chủ tạo Slider, Css aspect-ratio, Javascript-setInterval() #4

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,6 +3,10 @@ for (let index = 0; index < sliderItem.length; index++) {
     sliderItem[index].style.left = index * 100 + "%"
 }
 
+// Click chuyển ảnh 
+function silderMove(i){
+    sliderItems.style.transform = `translateX(-${i * 100}%)`
+}
 const sliderItems = document.querySelector('.slider-items')
 const arrowRight = document.querySelector('.ri-arrow-right-line')
 const arrowLeft = document.querySelector('.ri-arrow-left-line')
@@ -10,16 +14,29 @@ let i = 0
 arrowRight.addEventListener('click',()=>{
     i++
     if(i < sliderItem.length){
-        sliderItems.style.transform = `translateX(-${i * 100}%)`
+        silderMove(i)
     }else{
-        i = sliderItem.length - 1 //Giữ i ở giới hạn trên
+        i = sliderItem.length - 1 
     }
 })
 arrowLeft.addEventListener('click',()=>{
     if(i > 0){
         i--
-        sliderItems.style.transform = `translateX(-${i * 100}%)`
+        silderMove(i)
     }else{
-        i = 0 // Giữ i ở giới hạn dưới
+        i = 0 
     }
 })
+
+// Di chuyển ảnh tự động
+// function autoSlider(){
+//     if(i<sliderItem.length-1)
+//     {
+//         i++
+//         silderMove(i)
+//     }else{
+//         i = 0
+//         silderMove(i)
+//     }
+// }
+// setInterval(autoSlider,5000)
