70794766666a43ecaf5053cc5811658b78b88586
nguyenvanduydev001 <nguyenvanduydev001@gmail.com>
2024-06-12 10:24:12 +0700

    Giao diện trang chủ,Tạo danh sách sản phẩm Hot với Html, Css - Grid #6

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,48 +1,47 @@
-const sliderItem = document.querySelectorAll('.slider-item')
+const sliderItem = document.querySelectorAll(".slider-item");
 for (let index = 0; index < sliderItem.length; index++) {
-    sliderItem[index].style.left = index * 100 + "%"
+  sliderItem[index].style.left = index * 100 + "%";
 }
 
-// Click chuyển ảnh 
-function silderMove(i){
-    sliderItems.style.transform = `translateX(-${i * 100}%)`
+// Click chuyển ảnh
+function silderMove(i) {
+  sliderItems.style.transform = `translateX(-${i * 100}%)`;
 }
-const sliderItems = document.querySelector('.slider-items')
-const arrowRight = document.querySelector('.ri-arrow-right-line')
-const arrowLeft = document.querySelector('.ri-arrow-left-line')
-let i = 0
-arrowRight.addEventListener('click',()=>{
-    i++
-    if(i < sliderItem.length){
-        silderMove(i)
-    }else{
-        i = sliderItem.length - 1 
-    }
-})
-arrowLeft.addEventListener('click',()=>{
-    if(i > 0){
-        i--
-        silderMove(i)
-    }else{
-        i = 0 
-    }
-})
+const sliderItems = document.querySelector(".slider-items");
+const arrowRight = document.querySelector(".ri-arrow-right-line");
+const arrowLeft = document.querySelector(".ri-arrow-left-line");
+let i = 0;
+arrowRight.addEventListener("click", () => {
+  i++;
+  if (i < sliderItem.length) {
+    silderMove(i);
+  } else {
+    i = sliderItem.length - 1;
+  }
+});
+arrowLeft.addEventListener("click", () => {
+  if (i > 0) {
+    i--;
+    silderMove(i);
+  } else {
+    i = 0;
+  }
+});
 
 // Di chuyển ảnh tự động
-function autoSlider(){
-    if(i<sliderItem.length-1)
-    {
-        i++
-        silderMove(i)
-    }else{
-        i = 0
-        silderMove(i)
-    }
+function autoSlider() {
+  if (i < sliderItem.length - 1) {
+    i++;
+    silderMove(i);
+  } else {
+    i = 0;
+    silderMove(i);
+  }
 }
-setInterval(autoSlider,5000)
+setInterval(autoSlider, 5000);
 // Menubar responsive
-const Menubar = document.querySelector('.header-bar-icon')
-const headerNav = document.querySelector('.header-nav')
-Menubar.addEventListener('click',()=>{
-    headerNav.classList.toggle('active')
-})
+const Menubar = document.querySelector(".header-bar-icon");
+const headerNav = document.querySelector(".header-nav");
+Menubar.addEventListener("click", () => {
+  headerNav.classList.toggle("active");
+});
