de0fe69465e20f14308486b1add3f2a5279ec2ee
nguyenvanduydev001 <nguyenvanduydev001@gmail.com>
2024-06-15 22:36:02 +0700

    Giao diện trang Sản Phẩm, Show Ảnh Sản Phẩm với Html, Css, Javascript #10

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,47 +1,73 @@
-const sliderItem = document.querySelectorAll(".slider-item");
-for (let index = 0; index < sliderItem.length; index++) {
-  sliderItem[index].style.left = index * 100 + "%";
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const sliderItem = document.querySelectorAll(".slider-item");
+  const sliderItems = document.querySelector(".slider-items");
+  const arrowRight = document.querySelector(".ri-arrow-right-line");
+  const arrowLeft = document.querySelector(".ri-arrow-left-line");
+  const Menubar = document.querySelector(".header-bar-icon");
+  const headerNav = document.querySelector(".header-nav");
 
-// Click chuyển ảnh
-function silderMove(i) {
-  sliderItems.style.transform = `translateX(-${i * 100}%)`;
-}
-const sliderItems = document.querySelector(".slider-items");
-const arrowRight = document.querySelector(".ri-arrow-right-line");
-const arrowLeft = document.querySelector(".ri-arrow-left-line");
-let i = 0;
-arrowRight.addEventListener("click", () => {
-  i++;
-  if (i < sliderItem.length) {
-    silderMove(i);
-  } else {
-    i = sliderItem.length - 1;
+  if (sliderItems) {
+    for (let index = 0; index < sliderItem.length; index++) {
+      sliderItem[index].style.left = index * 100 + "%";
+    }
   }
-});
-arrowLeft.addEventListener("click", () => {
-  if (i > 0) {
-    i--;
-    silderMove(i);
-  } else {
-    i = 0;
-  }
-});
 
-// Di chuyển ảnh tự động
-function autoSlider() {
-  if (i < sliderItem.length - 1) {
-    i++;
-    silderMove(i);
-  } else {
-    i = 0;
-    silderMove(i);
+  //* Click chuyển ảnh
+  function sidlerMove(i) {
+    if (sliderItems) {
+      sliderItems.style.transform = `translateX(-${i * 100}%)`;
+    }
   }
-}
-setInterval(autoSlider, 5000);
-// Menubar responsive
-const Menubar = document.querySelector(".header-bar-icon");
-const headerNav = document.querySelector(".header-nav");
-Menubar.addEventListener("click", () => {
-  headerNav.classList.toggle("active");
+
+  let i = 0;
+  if (arrowRight && arrowLeft) {
+    arrowRight.addEventListener("click", () => {
+      i++;
+      if (i < sliderItem.length) {
+        sidlerMove(i);
+      } else {
+        i = sliderItem.length - 1;
+      }
+    });
+    arrowLeft.addEventListener("click", () => {
+      if (i > 0) {
+        i--;
+        sidlerMove(i);
+      } else {
+        i = 0;
+      }
+    });
+  }
+
+  //* Di chuyển ảnh tự động
+  function autoSlider() {
+    if (i < sliderItem.length - 1) {
+      i++;
+      sidlerMove(i);
+    } else {
+      i = 0;
+      sidlerMove(i);
+    }
+  }
+
+  setInterval(autoSlider, 5000);
+
+  //* Menubar responsive
+  if (Menubar && headerNav) {
+    Menubar.addEventListener("click", () => {
+      headerNav.classList.toggle("active");
+    });
+  }
 });
+// *click product image detail
+const imageSmall = document.querySelectorAll(".product-images-items img");
+const imageMain = document.querySelector(".main-image");
+for (let index = 0; index < imageSmall.length; index++) {
+  imageSmall[index].addEventListener("click", () => {
+    for (let a = 0; a < imageSmall.length; a++) {
+      imageSmall[a].classList.remove('active')
+    }
+    imageMain.src = imageSmall[index].src;
+    imageSmall[index].classList.add("active");
+  });
+}
